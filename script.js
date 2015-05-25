825560ff6dcaa25da0d5eaefc428dc0b5d7a9b50
Heather Booker <heather.booker@hotmail.com>
2015-05-24 20:16:26 -0700

    input fields and 'add to order' button

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-var canvas = document.getElementById('mycanvas'),
-    context = canvas.getContext('2d');
+var myOrder = [];
+
+function Food(itemName, itemPrice) {
+    this.item = itemName;
+    this.price = itemPrice;
+}
+
+var aFood = new Food('grapes', 10)
+console.log(aFood);
+myOrder.push(aFood);
+console.log(myOrder);
+
+function getInfo() {
+    var name = document.getElementById('foodName').value;
+    var cost = document.getElementById('foodPrice').value;
+    console.log(name, cost);
+}
